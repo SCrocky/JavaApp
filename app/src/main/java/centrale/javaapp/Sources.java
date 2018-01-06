d2f19cd46333513f8940773d0889679605ba6456
Sebastien C <cs_uat@memepr.com>
2018-01-06 18:30:34 +0100

    Création d'une classe de sources et modification de l'icone

---
+++ app/src/main/java/centrale/javaapp/Sources.java
@@ -0,0 +1,27 @@
+package centrale.javaapp;
+
+/**
+ * Created by Seb on 1/5/2018.
+ */
+public class Sources {
+
+    private String id;
+    private String name;
+    private String description;
+    private String url;
+    private String category;
+    private String language;
+
+    private Sources(String id, String name, String description, String url, String category, String language){
+        this.id =id;
+        this.name=name;
+        this.description = description;
+        this.url = url;
+        this.category = category;
+        this.language = language;
+    }
+
+
+
+
+}
